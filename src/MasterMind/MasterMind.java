12a602089d992d80a589164aec01393cfe6d8440
ELEVEF11 <heamatui@gmail.com>
2022-10-03 13:56:00 -1000

    Ajout Attributs et méthode nbCouleurInvalid

--- src/MasterMind/MasterMind.java
+++ src/MasterMind/MasterMind.java
@@ -3,5 +3,18 @@ package MasterMind;
 
 
 public class MasterMind {
+    private String[] couleurs = {"Acajou", "Brique", "Cyan", "Denim", "Ecru", "Fuchsia", "Gris", "Havane", "Indigo", "Jaune", "Kola", "Lavande"};
+    private String[] lettreCouleur = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
+    private String codeSecret = "";
+    private int nbColorTrue = 0;
+    private int nbColorOrder = 0;
     
+    
+    public void nbCouleurInvalid() {
+        if (codeSecret.length() > 4) {
+            System.out.println("Vous avez saisie trop de lettres !");
+        }else {
+            
+        }
+    }
 }
