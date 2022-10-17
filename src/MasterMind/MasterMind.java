e4c0e05d02f39d5baae5069c7982c64791fb8c81
tehea <heamatui@gmail.com>
2022-10-17 13:50:01 -1000

    Méthode genererCodeSecret fini

--- src/MasterMind/MasterMind.java
+++ src/MasterMind/MasterMind.java
@@ -1,15 +1,27 @@
 
 package MasterMind;
 
+import java.util.ArrayList;
+
 
 public class MasterMind {
-    private String[] couleurs = {"Acajou", "Brique", "Cyan", "Denim", "Ecru", "Fuchsia", "Gris", "Havane", "Indigo", "Jaune", "Kola", "Lavande"};
-    private String[] lettreCouleur = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
-    private String codeSecret = "";
-    private int nbColorTrue = 0;
-    private int nbColorOrder = 0;
+    private static String[] couleurs = {"Acajou", "Brique", "Cyan", "Denim", "Ecru", "Fuchsia", "Gris", "Havane", "Indigo", "Jaune", "Kola", "Lavande"};
+    private static ArrayList lettreCouleur = new ArrayList<String>(Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"));
+    private static String codeSecret = "";
+    private static int nbColorTrue = 0;
+    private static int nbColorOrder = 0;
     // on nesaie un truc bidon
     
+    // METTHODE qui GENERE le CODE SECRET
+    public static String genererCodeSecret() {
+        ArrayList<String> newList = lettreCouleur;
+        for (int nb = 1; nb <= 4; nb++) {
+            int index = (int) Math.floor(Math.random() * newList.size());
+            codeSecret += newList.get(index);
+            newList.remove(index);
+        }      
+        return codeSecret;
+    }
     
     public void nbCouleurInvalid() {
         if (codeSecret.length() > 4) {
---
+++ src/MasterMind/Test.java
@@ -0,0 +1,14 @@
+
+package MasterMind;
+
+
+public class Test {
+
+  
+    public static void main(String[] args) {
+        
+        System.out.println(MasterMind.genererCodeSecret());
+        
+    }
+    
+}
